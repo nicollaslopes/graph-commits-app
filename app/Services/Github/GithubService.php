3d4c41a8ed5b92af7fd49132c25d1e558b2bb900
nicollaslopes <nicollaslopesn@gmail.com>
2023-03-02 15:47:04 -0300

    creating method to return an data commits array

--- app/Services/Github/GithubService.php
+++ app/Services/Github/GithubService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services\Github;
 
+use Illuminate\Http\Client\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Http;
 
@@ -23,5 +24,32 @@ class GithubService
         }
 
         return $arrayNameRepositories;
+    }
+
+    public static function getDataCommits($repositoryName): array
+    {
+        $githubAttributes = Auth::User()->getAttributes();
+        $githubNickname = $githubAttributes['nickname'];
+
+        $commitsResponse = Http::get("https://api.github.com/repos/$githubNickname/$repositoryName/commits");
+        $commitsResponseArray = json_decode($commitsResponse->getBody());
+
+        $arrayCommits = [];
+        $arrayCommits['name'] = $repositoryName;
+
+        $dateToday = new \Datetime();
+
+        foreach ($commitsResponseArray as $keyDate => $commit) {
+            $dateCommit = date('d-m-Y', strtotime($commit->commit->committer->date));
+            $dateCommitFormat = new \Datetime($dateCommit);
+
+            $interval = $dateToday->diff($dateCommitFormat);
+
+            if ($interval->days < 90) {
+                $arrayCommits['date'][$keyDate] = $dateCommit;
+            }
+        }
+
+        return $arrayCommits;
     }
 }

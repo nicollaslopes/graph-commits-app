f355f9ad3e446fb782ff541654a545d1e2747ca7
nicollaslopes <nicollaslopesn@gmail.com>
2023-03-03 22:11:37 -0300

    graph js

--- resources/views/graphs.blade.php
+++ resources/views/graphs.blade.php
@@ -18,10 +18,11 @@
                     <center>
                         <h2>Este repositório não possui commits nos últimos 90 dias.</h2>
                     </center>
-                    @endif
+                    @else
                     <center>
                         Exibindo a data dos commits dos últimos 90 dias do repositório <b> {{ $arrayCommits['name'] }} </b>
                     </center>
+                    @endif
                     <a href="{{ url()->previous() }}" class="btn btn-primary float-right">
                         Voltar
                     </a>
@@ -34,4 +35,56 @@
     </div>
 </x-app-layout>
 
-<script src="{{ mix('js/graphsCommit.js') }}"></script>
+<script>
+    const arr = @json($arrayCommits);
+    const arrFormated = arr.date;
+    const arrCountDates = [];
+
+    arrFormated.forEach(function (x) { arrCountDates[x] = (arrCountDates[x] || 0) + 1; });
+
+    const eixoX = [];
+    const eixoY = [];
+
+    Object.keys(arrCountDates).forEach(key => {
+        eixoX.push(key)
+        eixoY.push(arrCountDates[key])
+    });
+
+    const ctx = document.getElementById('myChart');
+    const myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: eixoX,
+            datasets: [{
+                label: 'Commits',
+                data: eixoY,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+</script>
